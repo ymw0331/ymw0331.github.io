1678267476e5fba5292b59b822b856f91557a0f4
Wayne Yong <ymw0331@gmail.com>
2023-03-02 21:32:45 +0800

    added works

--- frontend_react/src/client.js
+++ frontend_react/src/client.js
@@ -5,10 +5,9 @@ export const client = sanityClient( {
   projectId: process.env.REACT_APP_SANITY_PROJECT_ID,
   dataset: 'production',
   apiVersion: '2022-02-01',
-  useCdn: 'true',
-  token: process.env.REACT_APP_SANITY_TOKEN
+  useCdn: true,
+  token: process.env.REACT_APP_SANITY_TOKEN,
 } );
-
 
 const builder = imageUrlBuilder( client );
 
