b9132b7b31b4dd5f7e25895f81e2e50a73c6ad8a
Wayne Yong <ymw0331@gmail.com>
2024-07-21 10:34:22 +0800

    change latest resume

--- src/container/Header/Header.jsx
+++ src/container/Header/Header.jsx
@@ -5,7 +5,7 @@ import Typed from 'typed.js';
 import { images } from '../../constants';
 import "./Header.scss";
 import { NavLink } from "react-router-dom";
-import pdfFile from '../../assets/MY_YONG MING WEI (Wayne)_090624.pdf';
+import pdfFile from '../../assets/MY_YONG MING WEI_190724.pdf';
 
 
 const Header = () => {
